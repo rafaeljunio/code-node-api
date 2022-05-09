5a30c75405deb52af39b5d7832c9964292ba0a2f
Rafael Souza <rafael.souza@animasales.com.br>
2022-05-09 10:31:32 -0300

    chore: add jest

---
+++ jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  roots: ['<rootDir>/src'],
+  clearMocks: true,
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  testEnvironment: 'node',
+  transform: {
+    '.+\\.ts$': 'ts-jest'
+  }
+}
